b0cb016722a5c0a167d3a23852abc404068220aa
Wagner Ferreira Costa <wagner.costa@zema.com>
2023-03-06 13:14:05 -0300

    :sparkles: feat: validando insercao de novos usuarios

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { Container, Form, FormError, Header } from './styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '../../lib/axios'
+import AxiosError from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -46,6 +47,9 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+      }
       console.log(err)
     }
   }

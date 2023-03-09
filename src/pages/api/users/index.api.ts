377204f604a02ff2b814ecc04151fb8cfed428b0
Wagner Ferreira Costa <wagner.costa@zema.com>
2023-03-09 08:28:26 -0300

    :bug: fix: correcao register users

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -3,38 +3,38 @@ import { prisma } from '../../../lib/prisma'
 import { setCookie } from 'nookies'
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    if (req.method !== 'POST') {
-        return res.status(405).end()
-    }
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
 
-    const { name, username } = req.body
+  const { name, username } = req.body
 
-    const userExists = await prisma.user.findUnique({
-        where: {
-            username,
-        },
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already taken.',
     })
+  }
 
-    if (userExists) {
-        return res.status(400).json({
-            message: 'Username already taken.',
-        })
-    }
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            username,
-        },
-    })
+  setCookie({ res }, 'ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    path: '/',
+  })
 
-    setCookie({ res }, 'ignitecall:userId', user.id, {
-        maxAge: 60 * 60 * 24 * 7, // 7 dias
-        path: '/',
-    })
-
-    return res.status(201).json(user)
+  return res.status(201).json(user)
 }

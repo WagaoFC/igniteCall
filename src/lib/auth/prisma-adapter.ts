0537ec1ca4e72c425ccf36d931f77cf616d5fcfe
Wagner Ferreira Costa <wagner.costa@zema.com>
2023-03-16 12:26:59 -0300

    :ok_hand: style: ajustes lint

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -2,41 +2,41 @@ import { Adapter } from 'next-auth/adapters'
 import { prisma } from '../prisma'
 
 export function PrismaAdapter(): Adapter {
-    return {
-        async createUser(user) { },
+  return {
+    async createUser(user) {},
 
-        async getUser(id) {
-            const user = await prisma.user.findFirstOrThrow({
-                where: {
-                    id,
-                },
-            })
-
-            return {
-                id: user.id,
-                name: user.name,
-                username: user.username,
-                email: user.email!,
-                emailVerified: null,
-                avatar_url: user.avatar_url!,
-            }
+    async getUser(id) {
+      const user = await prisma.user.findFirstOrThrow({
+        where: {
+          id,
         },
+      })
 
-        async getUserByEmail(email) {
-            const user = await prisma.user.findFirstOrThrow({
-                where: {
-                    email,
-                },
-            })
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
 
-            return {
-                id: user.id,
-                name: user.name,
-                username: user.username,
-                email: user.email!,
-                emailVerified: null,
-                avatar_url: user.avatar_url!,
-            }
+    async getUserByEmail(email) {
+      const user = await prisma.user.findFirstOrThrow({
+        where: {
+          email,
         },
-    }
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
+  }
 }

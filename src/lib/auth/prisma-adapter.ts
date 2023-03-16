2208a0730e1aeb9a8135592ca998198da850fdf7
Wagner Ferreira Costa <wagner.costa@zema.com>
2023-03-16 12:17:47 -0300

    :sparkles: feat: create adapter prisma

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,42 @@
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(): Adapter {
+    return {
+        async createUser(user) { },
+
+        async getUser(id) {
+            const user = await prisma.user.findFirstOrThrow({
+                where: {
+                    id,
+                },
+            })
+
+            return {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email!,
+                emailVerified: null,
+                avatar_url: user.avatar_url!,
+            }
+        },
+
+        async getUserByEmail(email) {
+            const user = await prisma.user.findFirstOrThrow({
+                where: {
+                    email,
+                },
+            })
+
+            return {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email!,
+                emailVerified: null,
+                avatar_url: user.avatar_url!,
+            }
+        },
+    }
+}
